b0a6958099c052ddd96292e34985a4efa47e5a40
Senan <senan996@gmail.com>
2022-02-08 22:23:12 +0100

    Login devuelve usuario

--- routes/users.js
+++ routes/users.js
@@ -122,7 +122,7 @@ module.exports = function (app, gestorBD) {
         "usuarios",
         function (usuario) {
           if (usuario.length != 0) {
-            res.send({ status: 200, data: { isVerified } });
+            res.send({ status: 200, data: { isVerified, usuario } });
           } else {
             const nuevoUsuario = {
               email: req.params.email,
