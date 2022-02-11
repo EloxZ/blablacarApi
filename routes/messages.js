3cb16fa735db68f7ef66958c5011888b951bdb23
Senan <senan996@gmail.com>
2022-02-11 22:12:35 +0100

    Conversaciones

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,4 @@
 module.exports = function (app, gestorBD) {
-    
-   
-
     app.get("/messages", function (req, res) {
         gestorBD.obtenerItem({}, 'mensajes', function (mensajes) {
             if (mensajes == null) {
@@ -13,13 +10,9 @@ module.exports = function (app, gestorBD) {
     });
 
     app.post('/messages', function (req, res) {
-        //TODO hacer validador y encriptar la contraseña
-        console.log(req.body);
         var cuerpo = req.body;
         cuerpo.stamp = new Date();
-        
-        console.log(cuerpo);
-        gestorBD.insertarItem(req.body, 'mensajes', function (mensaje) {
+        gestorBD.insertarItem(cuerpo, 'mensajes', function (mensaje) {
             if (mensaje == null) {
                 console.log("WARN: Fallo al insertar un mensaje")
                 res.send({ Error: { status: 500, data: "Se ha producido un error al insertar el mensaje, intentelo de nuevo más tarde" } })
@@ -52,18 +45,4 @@ module.exports = function (app, gestorBD) {
                 res.send({status: 200, data: {mensaje: mensaje}})            }
         });        
     })
-    
-    app.put('/messages/:id', function (req, res) {
-        console.log(req.body);
-        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-        let nuevoMensaje = req.body;
-        gestorBD.modificarItem(criterio, nuevoMensaje, 'mensajes', function(result){
-            if (result==null)
-                res.send({ Error: { status: 500, data: "Se ha producido un error al editar el mensaje, intentelo de nuevo más tarde" } })
-            else {
-                res.send({status: 200, data: {msg: 'Mensaje editado correctamente'}})
-            }
-        })
-    });
-
 }
